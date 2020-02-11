c19b6776259ca5b17a3217ba5fecd132d3ad8a0d
Koldunova Alexandra <40992427+Koldunova@users.noreply.github.com>
2020-02-11 18:14:40 +0300

    fix bug connected with save groups in DB

--- Form4.cs
+++ Form4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,27 +23,48 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dataGridView1.AllowUserToAddRows = false;
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {  /* dataGridView1.AllowUserToAddRows = false;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    XElement root = new XElement("group");
+                    for (int i = 0; i < dataGridView1.RowCount; i++)
+                    {
+                        XElement s = new XElement("student");
+                        XElement n = new XElement("name", Convert.ToString(dataGridView1.Rows[i].Cells[0].Value));
+                        s.Add(n);
+                        root.Add(s);
+                    }
+                    XDocument doc = new XDocument();
+                    doc.Add(root);
+                    doc.Save(saveFileDialog1.OpenFile());
+              
+
+            dataGridView1.AllowUserToAddRows = true;*/
+            };
+            //сохранить в бд
+            if (textBox1.Text.Length > 0)
             {
-                XElement root = new XElement("group");
+                string sql = "";
+                OleDbCommand command;
                 for (int i = 0; i < dataGridView1.RowCount-1; i++)
                 {
-                    XElement s = new XElement("student");
-                    XElement n = new XElement("name", Convert.ToString(dataGridView1.Rows[i].Cells[0].Value));
-                    s.Add(n);
-                    root.Add(s);
+                    sql = "INSERT INTO students ([nameStudent], [group]) VALUES ('" + Convert.ToString(dataGridView1.Rows[i].Cells[0].Value) + "', '" + textBox1.Text + "'); ";
+                    command = new OleDbCommand(sql, f.myConnection);
+                    command.ExecuteNonQuery();
                 }
-                XDocument doc = new XDocument();
-                doc.Add(root);
-                doc.Save(saveFileDialog1.OpenFile());
+                MessageBox.Show("Группа сохранена", "Успешно", MessageBoxButtons.OK);
+                dataGridView1.Rows.Clear();
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите группу", "Внимание", MessageBoxButtons.OK);
             }
 
-            dataGridView1.AllowUserToAddRows = true;
         }
         int kol = 0;
         private void button2_Click(object sender, EventArgs e)
-        {
+        {/*
             foreach (DataGridViewRow row in dataGridView1.Rows)
             {
                 foreach (DataGridViewCell cell in row.Cells)
@@ -55,22 +77,26 @@ namespace WindowsFormsApp1
                     }
                 }
             }
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                try
+                
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
-                    kol = 0;
-                    XDocument doc = XDocument.Load(openFileDialog1.OpenFile());
-                    foreach (XElement s in doc.Element("group").Elements("student"))
+                    try
                     {
-                        dataGridView1.Rows.Add(s.Element("name").Value);
-                        kol++;
+                        kol = 0;
+                        XDocument doc = XDocument.Load(openFileDialog1.OpenFile());
+                        foreach (XElement s in doc.Element("group").Elements("student"))
+                        {
+                            dataGridView1.Rows.Add(s.Element("name").Value);
+                            kol++;
+                        }
                     }
+                    catch (Exception) { }
+
                 }
-                catch (Exception) { }
-
+                
             }
-
+            */
         }
 
         private void Form4_Load(object sender, EventArgs e)
